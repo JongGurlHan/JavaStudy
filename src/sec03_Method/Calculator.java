c263acf97dde53352d44e258a0ca8cd62daaace4
JG HAN <gojkhan13@gmail.com>
2021-04-09 21:17:55 +0900

    0409 Method학습1

---
+++ src/sec03_Method/Calculator.java
@@ -0,0 +1,23 @@
+package sec03_Method;
+
+public class Calculator {
+
+    //Field
+    //Constructor
+    //Method
+    void powerOn(){
+        System.out.println("전원을 켭니다.");
+    }
+
+    int plus(int x, int y){
+        //int result = ;
+        return x + y;
+    }
+
+    double divide(int x, int y){
+        double result = (double) x / (double) y;
+        return result;
+    }
+
+
+}
