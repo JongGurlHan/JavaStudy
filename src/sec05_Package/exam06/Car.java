dbb2bf749bfe33dd1018063d3bebcc1e70fdf32b
JG HAN <gojkhan13@gmail.com>
2021-04-14 01:11:26 +0900

    0414 getter, setter 복습

---
+++ src/sec05_Package/exam06/Car.java
@@ -0,0 +1,31 @@
+package sec05_Package.exam06;
+
+public class Car {
+
+    private int speed;
+    private boolean stop;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed < 0){
+            this.speed = 0;
+            return;
+        }else{
+            this.speed =  0;
+        }
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+        if(stop){
+            speed = 0;
+        }
+    }
+}
